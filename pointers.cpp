0e4af18aee3c3953baf365e65bb0b872a5b78914
Sabir Mehdi <sabirmehdi528@gmail.com>
2025-11-28 23:03:39 +0500

    Pointer case study

--- pointers.cpp
+++ pointers.cpp
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <iostream>
 using namespace std;
 
@@ -17,18 +9,46 @@ struct Node
 
 int main(){
     
+  //Baisc access to value through pointer
     Node n1;
     Node *p;
     
-    p=&n1;
+    p=&n1; // one way of getting address of the location, other one is with key word 'new'.
     
     cout<<"Print with simple Struct method: ";
     cout<<n1.data<<endl;
     
     cout<<"Print with pointer 'p': ";
     cout<<(*p).data<<endl;
-    
+
     cout<<"Print with (->) method: ";
     cout<<p->data<<endl;
+    
+    cout<<"Notice the data is value is changed now: ";
+    (*p).data=20;
+    cout<<(*p).data<<endl;
+    
+    
+    // Creating new Node
+    
+    p= new Node;
+    
+    p->data= 30; // updating the data value of newly created node
+    
+    cout<<"This is address to new Node: "<<p<<endl; // prints the adress of new node
+    
+    cout<<"Data in New Node: "<<p->data<<endl; // prints the adress of new node
+    
+    
+    // Deleting an existing instance
+    
+    // using "delete" key word to mark the memory as free for OS.
+    
+    delete p;
+    
+    cout<<p;
+    
+    
+
     
 }
